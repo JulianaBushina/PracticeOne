aa922ce7f9d32cc0bc6dd5e8951ebf36aea40530
JulianaBushina <bushina_yv@mail.ru>
2021-10-17 19:19:23 +0300

    Initial commit

---
+++ clickbuttons/src/main/java/ru/mirea/bushina/clickbuttons/ButtonsActivity.java
@@ -0,0 +1,29 @@
+package ru.mirea.bushina.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ButtonsActivity extends AppCompatActivity {
+    private TextView textViewOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+        textViewOut = findViewById(R.id.textViewOut);
+    }
+
+    public void clickedButtonOK(View view) {
+        textViewOut.setText(R.string.clicked_buttno_ok);
+        Toast.makeText(this, R.string.clicked_buttno_ok, Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickedButtonCancel(View view) {
+        textViewOut.setText(R.string.clicked_button_cancel);
+        Toast.makeText(this, R.string.clicked_button_cancel, Toast.LENGTH_SHORT).show();
+    }
+}
